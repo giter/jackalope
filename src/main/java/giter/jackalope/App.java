c873875b5740be9be9f30bcabbd9e6fd43d9b60e
giter <nubix@qq.com>
2013-04-11 07:32:04 +0800

    refactory

--- src/main/java/giter/jackalope/App.java
+++ src/main/java/giter/jackalope/App.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
@@ -21,43 +20,36 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class App {
 
-  static Pattern RESOURCES = Pattern.compile("(css|js|png|jpg|gif|swf)$");
-
   public static void main(String[] args) throws Exception {
 
     Server server = new Server(8080);
 
-    final ResourceHandler resources = new ResourceHandler();
-    resources.setDirectoriesListed(false);
-    resources.setBaseResource(Resource.newClassPathResource("/web"));
+    final ResourceHandler resources = new ResourceHandler() {
 
-    HandlerCollection handlers = new HandlerCollection() {
+      Pattern RESOURCES = Pattern.compile("(css|js|png|jpg|gif|swf)$");
 
       @Override
       public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
           throws IOException, ServletException {
 
-        if (!isStarted()) return;
-
         if (RESOURCES.matcher(target).find()) {
-          resources.handle(target, baseRequest, request, response);
+          super.handle(target, baseRequest, request, response);
           return;
         }
-
-        Handler[] handlers = getHandlers();
-        if (handlers == null) return;
-        for (Handler handler : handlers) {
-          handler.handle(target, baseRequest, request, response);
-          if (baseRequest.isHandled()) return;
-        }
-
       }
     };
+
+    resources.setDirectoriesListed(false);
+    resources.setBaseResource(Resource.newClassPathResource("/web"));
+
+    HandlerCollection handlers = new HandlerCollection();
 
     WebAppContext wac = new WebAppContext();
     AnnotationConfiguration conf = new ClassPathAnnotationConfiguration("/");
     wac.setConfigurations(new Configuration[] { conf });
     wac.setContextPath("/");
+
+    handlers.addHandler(resources);
     handlers.addHandler(wac);
 
     server.setHandler(handlers);
