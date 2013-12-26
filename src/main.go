e967dafb9285b741a0438112b0d7f70d56feadb5
giter <nubix@qq.com>
2013-12-26 22:19:47 +0800

    delete libs/mgo

--- src/main.go
+++ src/main.go
@@ -1,11 +1,36 @@
-package src
+package main
 
 import (
 	"fmt"
+	"mgo"
+	"mgo/bson"
 )
 
 func main() {
 
-	fmt.Println("Hello World!");
+
+	m,err := mgo.Dial("127.0.0.1");
+	
+	if err != nil { 
+		panic(err); 
+	}
+	
+	defer m.Close();
+	
+	db := m.DB("test");
+	c  := db.C("test");
+	
+	c.Insert(bson.M(bson.M{"A":"111"}));
+	
+	r := bson.M{};
+	
+	query := c.Find(bson.M{});
+	
+	iter:= query.Iter();
+	
+	for iter.Next(&r) {
+		fmt.Println(r);
+	}
+	
 }
 
